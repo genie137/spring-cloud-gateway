83dced25c7abf46c0c61290b864f47fe11ea71a7
Joris Oosterhuis <joris@easthome.nl>
2025-06-07 20:29:23 +0200

    First commit

---
+++ spring-cloud-gateway-server-mvc/src/main/java/org/springframework/cloud/gateway/server/mvc/filter/global/GlobalHandlerFilterFunction.java
@@ -0,0 +1,31 @@
+package org.springframework.cloud.gateway.server.mvc.filter.global;
+
+import org.springframework.web.servlet.function.HandlerFilterFunction;
+import org.springframework.web.servlet.function.HandlerFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+public abstract class GlobalHandlerFilterFunction implements HandlerFilterFunction<ServerResponse, ServerResponse> {
+
+	public GlobalHandlerFilterFunction() {
+
+	}
+
+	public ServerResponse doFilter(ServerRequest request, HandlerFunction<ServerResponse> next) throws Exception {
+		return doFilterOnResponse(next.handle(doFilterOnRequest(request)));
+	}
+
+
+	public ServerRequest doFilterOnRequest(ServerRequest request) {
+		return request;
+	}
+
+	public ServerResponse doFilterOnResponse(ServerResponse response) {
+		return response;
+	}
+
+	@Override
+	public ServerResponse filter(ServerRequest request, HandlerFunction<ServerResponse> next) throws Exception {
+		return doFilter(request,next);
+	}
+}
